abd247dbba25eb798360b42c5f30d4f4e6c944ed
Jalalbii <8451031656safa@gmail.com>
2025-10-20 17:18:45 +0530

    api integrated

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,29 @@
-import './App.css'
+import React, { useEffect, useState } from 'react';
 
 function App() {
+  const [items, setItems] = useState([]);
+  const [error, setError] = useState('');
+
+  useEffect(() => {
+    fetch(`${import.meta.env.VITE_API_URL}/api/items`)
+      .then((res) => res.json())
+      .then((res) => {
+        setItems(res?.data?.items)
+      })
+      .catch(() => setError('Failed to fetch items'));
+  }, []);
 
   return (
-    <>
-     App
-    </>
-  )
+    <div style={{ padding: '2rem', fontFamily: 'Arial' }}>
+      <h1 style={{border: "5px solid pink"}}>🚀 Deployed from FEATURE branch</h1>
+      {error && <p style={{ color: 'red' }}>{error}</p>}
+      <ul>
+        {items?.map((item, idx) => (
+          <li key={item._id || idx}>{item.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
-export default App
+export default App;
